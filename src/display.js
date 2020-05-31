29801811ee89a25ea8844eaa65a8c6590840239a
blackstate <josemiguelmneyra@gmail.com>
2020-05-31 14:44:50 +0800

    add project, choose todo, change project, local storage, and add todo added

---
+++ src/display.js
@@ -0,0 +1,161 @@
+import todoList from './todoList'
+
+const display = (function () {
+    const projectList = document.querySelector('#projectList');
+    const todos = document.querySelector('#todo-list')
+
+    let titleElement = document.querySelector('#title');
+    let descriptionElement = document.querySelector('#description');
+    let dueDateElement = document.querySelector('#date');
+    let priorityElement = document.querySelector('#priority');
+
+    let currentHeader = document.querySelector('.current-project');
+    let todoDetails = document.querySelector('.todo-details');
+
+    let current = todoList.getCurrent();
+
+    /* ---------------------------------------------------- */
+
+    const changeProject = (name) => {
+        todoList.changeCurrent(name);
+        render();
+    }
+    
+    const clearProjectList = () => {
+        projectList.innerHTML = "";
+    }
+
+    const clearProjectTodos = () => {
+        todos.innerHTML = "";
+    }
+
+    const clearTodoDetails = () => {
+        todoDetails.innerHTML = "";
+    }
+
+    const isCurrentProject = (project) => {
+        current = todoList.getCurrent();
+        if (project == current) {
+            return true;
+        }
+    }
+
+    const showProjects = () => {
+        clearProjectList();
+        clearTodoDetails();
+        let projects = todoList.getAllProjects();
+        projects.forEach(project => {
+            
+            let createProjectItem = () =>{
+                let li = document.createElement('li');
+                li.addEventListener("click", () => {
+                    changeProject(project);
+                })
+                return li;
+            }
+
+            let projectItem = createProjectItem();
+            if (isCurrentProject(project) == true) {
+                projectItem.classList.add('selected')
+            }
+            projectItem.innerHTML = project;
+            projectList.appendChild(projectItem);
+            })
+    } 
+
+    const updateProjectHeader = () => {
+        currentHeader.innerHTML = current;
+    }
+
+    const viewTodo = (todo) => {
+        todoDetails.innerHTML = todo.getTitle();
+    }
+
+    const showProjectTodos = () => {
+        clearProjectTodos();
+        let projectTodos = todoList.getAllTodos();
+        projectTodos.forEach(todo => {
+            let todoItem = document.createElement('li');
+            todoItem.addEventListener('click', () => {
+                viewTodo(todo);
+            })
+            todoItem.innerHTML = todo.getTitle();
+            todos.appendChild(todoItem);
+        })
+
+        updateProjectHeader();
+    }
+
+    let resetAddFields = () => {
+        titleElement.value = descriptionElement.value = "";
+        dueDateElement.valueAsDate = new Date(); 
+    }
+
+    // reads the value in the add todo form
+    const getTodoDetails = () => {
+        let title = titleElement.value;
+        let description = descriptionElement.value;
+        let dueDate = dueDateElement.value;
+        let priority = priorityElement.value;
+
+        
+        if (title != "" && priority != "" && dueDate != "") {
+            resetAddFields();
+            return {title, description, dueDate, priority};
+        }
+
+        else {
+            return 0
+        } 
+    }
+
+    const addTodo =  () => {
+        let details = getTodoDetails();
+        if (details) {
+            todoList.addTodo(details);
+        }
+        showProjectTodos();
+    }
+
+    const addProject = () => {
+        const projectName = document.querySelector('#projectName');
+        todoList.addProject(projectName.value);
+        changeProject(projectName.value);
+        projectName.value = "";
+        showProjects();
+    }
+
+    const renderButtons = () => {
+        const addTodoButton = document.querySelector('#addTodo');
+        const addProjectButton = document.querySelector('#addProject');
+
+        addTodoButton.addEventListener('click', addTodo);
+        addProjectButton.addEventListener('click', addProject);
+    }
+
+    const render = () => {
+        showProjects();
+        showProjectTodos();
+        renderButtons();
+    }
+    
+    return {
+        render
+    }
+})(); 
+
+(function () {
+    let dueDateElement = document.querySelector('#date');   
+    dueDateElement.valueAsDate = new Date(); 
+})();
+
+
+// todoList.addTodo({title: "Sleep", description: "I will sleep", dueDate: "Today", priority: 3});
+// todoList.addTodo({title: "Eat", description: "I will eat", dueDate: "Today", priority: 2});
+
+// todoList.changeCurrent("Eyaw world");
+// todoList.addTodo({title: "Eyaw", description: "blaise benta", dueDate: "Tomorrow", priority: 69})
+// todoList.updateTodo("Sleep", "priority", 69);
+// todoList.showTodo("Sleep");
+
+export default display;
---
+++ src/todoList.js
@@ -0,0 +1,153 @@
+const todoList = (function () {
+    // factory
+    const Todo = (details) => {
+        const getTitle = () => details.title;
+    
+        const getDetails = () => {
+            return details
+        }
+        
+        const changeDetail  = (detail, value) =>  { 
+            switch (detail) {
+                case 'title':
+                    details.title = value;
+                    break;
+                case 'description':
+                    details.description = value;
+                    break;
+                case 'dueDate':
+                    details.dueDate = value;
+                    break;
+                case 'priority':
+                    details.priority = value;
+                    break;
+            }
+            saveLocal();
+        }
+        
+        return {
+            getTitle, getDetails, changeDetail
+        }
+    }
+
+    // handles storage 
+    const storage = (function () {
+        // saves todo list in localstorage with each todo's details
+        const save = () => {
+            let projectNames = Object.keys(projects);
+            let projectTodos = Object.values(projects);
+
+            let object = {};
+            // iterate through projects
+            for(let i=0; i<projectNames.length; i++) {
+                // set i'th key to project name
+                object[projectNames[i]] = [];
+                // iterate through todos and return their details
+                for(let k=0; k<projectTodos[i].length; k++) {
+                    object[projectNames[i]].push(projectTodos[i][k].getDetails());
+                }
+            }
+            localStorage.setItem("userProjects", JSON.stringify(object));
+        }
+
+        // gets todo list in localstorage then convert todo using its details using Todo factory
+        function get () {
+            // get object from localstorage
+            let object = JSON.parse(localStorage.getItem("userProjects"));
+            let keys = Object.keys(object);
+
+            //iterate through the keys(todos) of each project and use their details to convert them to a proper 'Todo'
+            for(let i=0; i<keys.length; i++) {
+                for(let k=0; k<object[keys[i]].length; k++) {
+                    let todoProper = Todo(object[keys[i]][k]);
+                    object[keys[i]][k] = todoProper;
+                }
+            }
+            return object;
+        }
+
+        // check if todolist exists in local storage, if not create one
+        function check () {
+            if (localStorage.getItem('userProjects') === null) {
+                let userProjects = {
+                    "Today": [],
+                    "Test Project": []
+                }
+        
+                localStorage.setItem("userProjects", JSON.stringify(userProjects))
+            }
+        }
+        
+        return {save, get, check}
+    })();
+
+    // check if local storage is present
+    storage.check();
+
+    /* ---------------------------------------------------- */
+
+    // get the localstorage values for the projects
+    let projects = storage.get();
+
+    // default current project is Today
+    let currentProject = projects["Today"];
+    
+    // return project names
+    const getAllProjects = () => Object.keys(projects);
+
+    // return todos of current Project
+    const getAllTodos = () => currentProject;
+
+    const getProject = (projectName) => {
+        return(projects[projectName])
+    }
+
+    const addProject = (projectName) => {
+        // if project already exists return
+        if (projects.hasOwnProperty(projectName)) {
+            console.log("duplicate project name!")
+            return;
+        }
+
+        projects[projectName] = [];
+        storage.save();
+    }
+
+    const _findTodo = (todoTitle) => {
+        for (i=0; i<currentProject.length; i++) {
+            if (currentProject[i].getTitle() == todoTitle) {
+                return currentProject[i];
+            }
+        }
+    }
+
+    // not needed at the moment
+    const showTodo = (todoTitle) => {
+        console.log(_findTodo(todoTitle).getDetails());
+    }
+    
+    const addTodo = (title, description, dueDate, priority) => {
+        currentProject.push(Todo(title, description, dueDate, priority));
+        storage.save();
+    }
+
+    const updateTodo = (todoTitle, detail, value) => {
+        _findTodo(todoTitle).changeDetail(detail, value);
+        storage.save();
+    }
+
+    const changeCurrent = (value) => {
+        currentProject = projects[value];
+    }
+
+    const getCurrent = () => {
+        return Object.keys(projects).find(key => projects[key] === currentProject);
+    }
+
+    return {
+        addTodo, getProject, updateTodo, showTodo, getAllProjects, changeCurrent,
+        getCurrent, getAllTodos, addProject
+    }
+})();
+
+export default todoList;
